25dbef3fb1266d5aed3af9c4c6d39372793bc1bc
Ricardo <r.pineu@campus.fct.unl.pt>
2025-10-07 21:58:15 +0100

    Aircraft class has now it's attributes.

--- src/Aircraft.java
+++ src/Aircraft.java
@@ -1,2 +1,11 @@
 public class Aircraft {
+    private String manufacturer;
+    private String model;
+    private int capacity;
+
+    public Aircraft(String manufacturer, String model, int capacity) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.capacity = capacity;
+    }
 }
