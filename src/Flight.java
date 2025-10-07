ce455981039f834d63016b274af76b15afd7a0fb
Ricardo <r.pineu@campus.fct.unl.pt>
2025-10-07 21:46:48 +0100

    Flight class has now it's attributes.

---
+++ src/Flight.java
@@ -0,0 +1,16 @@
+public class Flight {
+    private int flightId;
+    private String destination;
+    private String departureTime;
+    private int boardingDoor;
+    private String airline;
+    private int baseCost;
+
+    public Flight(int flyId, String destination, String departureTime, int boardingDoor, int baseCost) {
+        this.flightId = flyId;
+        this.destination = destination;
+        this.departureTime = departureTime;
+        this.boardingDoor = boardingDoor;
+        this.baseCost = baseCost;
+    }
+}
