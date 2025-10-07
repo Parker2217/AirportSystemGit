7a23119881e2e5eb3137cd5b76e3bd9c69340cf7
Ricardo <r.pineu@campus.fct.unl.pt>
2025-10-07 22:01:20 +0100

    Passenger class has now it's attributes.

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,2 +1,11 @@
 public class Passenger {
+    private String name;
+    private String email;
+    private int id;
+
+    public Passenger(String name, String email, int id) {
+        this.name = name;
+        this.email = email;
+        this.id = id;
+    }
 }
