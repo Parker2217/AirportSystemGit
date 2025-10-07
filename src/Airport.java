74034cd601ca003519af88603b8c4fc593dd585b
Ricardo <r.pineu@campus.fct.unl.pt>
2025-10-07 21:41:11 +0100

    Airport class has now it's attributes.

--- src/Airport.java
+++ src/Airport.java
@@ -1,4 +1,11 @@
 public class Airport {
     private String country;
     private String city;
+    private int tax;
+
+    public Airport(String country, String city, int tax) {
+        this.country = country;
+        this.city = city;
+        this.tax = tax;
+    }
 }
