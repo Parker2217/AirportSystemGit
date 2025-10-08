a81113d8965855710bc9dd9408ff18ff2e2a1eb4
Ricardo <r.pineu@campus.fct.unl.pt>
2025-10-08 23:16:03 +0100

    Names, numbers and emails of every member of the group added.

--- src/EManufacturer.java
+++ src/EManufacturer.java
@@ -1,3 +1,8 @@
+/**
+ * @author Daniel Costa Neves (68191) dc.neves@campus.fct.unl.pt
+ * @author Ricardo Afonso Caetano Pinéu (68561) r.pineu@campus.fct.unl.pt
+ * @author Rita Wu (68700) ri.wu@campus.fct.unl.pt
+ */
 public enum EManufacturer {
     Boeing, Airbus;
 }
